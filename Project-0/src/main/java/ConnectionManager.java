9249242995d7aa6f84d8fe37cf053c40bc0903e8
Tiffany Obi <tiffanyobi123@gmail.com>
2022-01-23 08:36:16 -0500

    configured connection to database

---
+++ Project-0/src/main/java/ConnectionManager.java
@@ -0,0 +1,50 @@
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class ConnectionManager {
+    public static ConnectionManager connectionManager;
+    public static Connection connection;
+
+
+    private ConnectionManager(){
+
+    }
+
+    private ConnectionManager connectionManager(){
+        if(connectionManager == null){
+            connectionManager = new ConnectionManager();
+        }
+        return connectionManager;
+    }
+
+    public static Connection getConnection(){
+        if(connection == null){
+            connection = connect();
+        }
+        return connection;
+    }
+
+    public static Connection connect(){
+        try {
+            Properties props = new Properties();
+            FileReader fr = new FileReader("src/main/resources/jdbc.properties");
+            props.load(fr);
+//            jdbc:mariadb://hostname:port/databaseName?user=username&password=password
+
+            String connectionURL = "jdbc:mariadb://" +
+                    props.get("hostname") + ":" +
+                    props.get("port") + "/" +
+                    props.get("dbname") + "?user=" +
+                    props.get("username") + "&password=" +
+                    props.get("password");
+//          Class.forName("org.mariadb.jdbc.Driver");
+            connection = DriverManager.getConnection(connectionURL);
+            System.out.println(connectionURL);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
