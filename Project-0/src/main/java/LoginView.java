de59872f4148dc86ad32757d104b89870d649ebb
Tiffany Obi <tiffanyobi123@gmail.com>
2022-01-24 18:58:10 -0500

    Adding Login View and Login Model

---
+++ Project-0/src/main/java/LoginView.java
@@ -0,0 +1,12 @@
+public class LoginView extends View {
+
+    public LoginView(){
+        viewName = "LoginView";
+        viewManager = ViewManager.getRenderer();
+    }
+    @Override
+    public void renderView() {
+        System.out.println("");
+
+    }
+}
