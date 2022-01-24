271e882f030e565a40d878ad68bcc8c77b46e7fd
Tiffany Obi <tiffanyobi123@gmail.com>
2022-01-24 18:57:22 -0500

    Adding View abstract class and View manager Class

--- Project-0/src/main/java/View.java
+++ Project-0/src/main/java/View.java
@@ -1,3 +1,10 @@
 public abstract class View {
+    protected String viewName;
+    protected ViewManager viewManager;
 
+    public String getViewName(){
+        return viewName;
+    }
+
+    public abstract void renderView();
 }
--- Project-0/src/main/java/ViewManager.java
+++ Project-0/src/main/java/ViewManager.java
@@ -1,2 +1,57 @@
+import java.util.Scanner;
+
+
 public class ViewManager {
+
+    private static ViewManager viewManager;
+    private boolean running;
+    public Scanner scanner;
+
+    CustomArrayList<View> viewList;
+    View nextView;
+
+    private ViewManager(){
+        running = true;
+        scanner = new Scanner(System.in);
+        viewList = new CustomArrayList<>();
+    }
+
+    public static ViewManager getRenderer(){
+        if (viewManager == null){
+            viewManager = new ViewManager();
+        }
+        return viewManager;
+    }
+
+    public void registerView(View view){
+        viewList.add(view);
+    }
+
+    public void navigate(String destination){
+        for(View view: viewList){
+            if(view.viewName.equals(destination)){
+                nextView = view;
+            }
+        }
+    }
+
+    public void renderView(){
+        nextView.renderView();
+    }
+
+
+    public Scanner getScanner(){
+        return scanner;
+    }
+
+    public boolean quit(){
+        return running = false;
+    }
+
+    public boolean isRunning(){
+        return running;
+    }
+
+
+
 }
