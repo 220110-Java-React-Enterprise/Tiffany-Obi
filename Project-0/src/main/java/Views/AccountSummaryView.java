0618047a824dc14a713be1715bda31ac0c8df502
Tiffany Obi <tiffanyobi123@gmail.com>
2022-01-26 16:41:31 -0500

    successfully showing accounts for current

---
+++ Project-0/src/main/java/Views/AccountSummaryView.java
@@ -0,0 +1,56 @@
+package Views;
+
+import Persistence.BankAccount;
+import Persistence.BankAccountData;
+import Persistence.Customer;
+import Utils.CurrentUser;
+import Utils.CustomArrayList;
+import Utils.ViewManager;
+
+public class AccountSummaryView extends View {
+
+    public AccountSummaryView(){
+        viewManager = ViewManager.getRenderer();
+        viewName = "AccountSummaryView";
+    }
+
+    @Override
+    public void renderView() {
+
+        BankAccountData bankData = new BankAccountData();
+        Customer customer = CurrentUser.getCurrentUser();
+
+        CustomArrayList<BankAccount> accounts = bankData.getCustomerAccounts(customer.getCustomerId());
+
+        System.out.println("====== ACCOUNT SUMMARY ======");
+
+        for(BankAccount account: accounts){
+            System.out.println("Account Number: "+ account.getAccountNumber() +
+                    "    Current Balance: " + account.getCheckingBalance());
+        }
+
+        System.out.println("  HOW CAN WE HELP YOU TODAY? ");
+        System.out.println(
+                "1) Return to Main Menu\n"+
+                        "2)Log Out \n"
+
+        );
+
+        String selection = viewManager.getScanner().nextLine();
+
+        switch (selection){
+            case "1":
+                viewManager.navigate("MainView");
+
+            case "2":
+
+                System.out.println("****************************");
+                System.out.println("*************Logging Out***************");
+                System.out.println("======= HAVE A GREAT DAY =======");
+
+
+                viewManager.quit();
+        }
+
+    }
+}
--- Project-0/src/main/java/Views/TransactionView.java
+++ Project-0/src/main/java/Views/TransactionView.java
@@ -1,6 +1,10 @@
 package Views;
 
+import Persistence.BankAccount;
+import Persistence.BankAccountData;
+import Persistence.Customer;
 import Utils.CurrentUser;
+import Utils.CustomArrayList;
 import Views.View;
 import Utils.ViewManager;
 
@@ -10,24 +14,96 @@ public class TransactionView extends View {
         viewName = "TransactionView";
         viewManager = ViewManager.getRenderer();
     }
+
     @Override
     public void renderView() {
 
-        System.out.println("we are here to make a " + CurrentUser.getTransactionType());
+        System.out.println("KEEP YOUR ACCOUNT SAFE");
+        System.out.println("****************************");
+        System.out.println("****************************");
+        System.out.println("****************************");
+        System.out.println("DON'T SHARE YOUR ACCOUNT INFORMATION WITH ANYONE");
 
 
-        viewManager.quit();
-//
-//        if (CurrentUser.getTransactionType() == "deposit"){
-//            System.out.println("Deposit logic");
-//            viewManager.quit();
-//        } else if (CurrentUser.getTransactionType() == "withdraw"){
-//            System.out.println("Withdraw logic");
-//            viewManager.quit();
-//        } else {
-//            System.out.println("Something is not adding up");
-//            viewManager.quit();
-//        }
+
+        if (CurrentUser.getTransactionType() == "deposit"){
+
+            BankAccountData bankData = new BankAccountData();
+            Customer customer = CurrentUser.getCurrentUser();
+
+            CustomArrayList<BankAccount> accounts = bankData.getCustomerAccounts(customer.getCustomerId());
+
+            System.out.println("Choose The Account To Deposit Into");
+
+            for(BankAccount account: accounts){
+                System.out.println("Account Number: "+ account.getAccountNumber() +
+                        "    Current Balance: " + account.getCheckingBalance());
+            }
+
+            System.out.println("Enter the account number");
+
+            Integer accountNumber = viewManager.getScanner().nextInt();
+            viewManager.getScanner().nextLine();
+
+            BankAccount selectedAccount = bankData.read(accountNumber);
+            System.out.println(selectedAccount.getAccountNumber() + " balance " + selectedAccount.getCheckingBalance());
+
+            System.out.println("HOW MUCH WOULD YOU LIKE TO DEPOSIT?");
+            Float deposit = viewManager.getScanner().nextFloat();
+            viewManager.getScanner().nextLine();
+
+
+            selectedAccount.deposit(deposit);
+
+            bankData.update(selectedAccount);
+
+            System.out.println("***********************************");
+            System.out.println("************Depositing*************");
+            System.out.println("***********************************");
+            System.out.println("============= S U C C E S S ===========");
+
+        viewManager.navigate("MainView");
+
+        } else if (CurrentUser.getTransactionType() == "withdraw"){
+
+            BankAccountData bankData = new BankAccountData();
+            Customer customer = CurrentUser.getCurrentUser();
+
+            CustomArrayList<BankAccount> accounts = bankData.getCustomerAccounts(customer.getCustomerId());
+
+            System.out.println("Choose The Account To Withdraw From");
+
+            for(BankAccount account: accounts){
+                System.out.println("Account Number: "+ account.getAccountNumber() +
+                        "    Current Balance: " + account.getCheckingBalance());
+            }
+
+            System.out.println("Enter the account number");
+
+            Integer accountNumber = viewManager.getScanner().nextInt();
+            viewManager.getScanner().nextLine();
+
+            BankAccount selectedAccount = bankData.read(accountNumber);
+            System.out.println(selectedAccount.getAccountNumber() + " balance " + selectedAccount.getCheckingBalance());
+
+            System.out.println("HOW MUCH WOULD YOU LIKE TO WITHDRAW?");
+            Float withdraw = viewManager.getScanner().nextFloat();
+            viewManager.getScanner().nextLine();
+
+//            Float newBalance = (selectedAccount.getCheckingBalance()+deposit);
+
+         if(selectedAccount.withdraw(withdraw)){
+                bankData.update(selectedAccount);
+            } else {
+             System.out.println("INSUFFICIENT FUNDS! Try again");
+             renderView();
+         }
+
+         viewManager.navigate("MainView");
+        } else {
+            System.out.println("Something is not adding up");
+            viewManager.quit();
+        }
 
 
     }
